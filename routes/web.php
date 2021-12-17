154c38c856ec4f4f9f319b95b085ada377747680
Diego Soto <Diego.soto3003@gmail.com>
2021-12-16 22:51:40 -0500

    Vista index y botones de acción

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/posts', Backend\PostController::class)
+    ->middleware('auth')
+    ->except('show');
